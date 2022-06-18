985a36f4c900281b0e8b9bb56f8001b8ffe9ddfc
Betr <xj19971@gmail.com>
2022-06-18 16:21:26 +0800

    test

---
+++ Assets/Scripts/UI/FightNumber/ShowFightNumberTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class ShowFightNumberTest : MonoBehaviour
+{
+
+    private WaitForSeconds wait;
+
+    [Range(0.01f, 1f)]
+    public float interval = .1f;
+
+
+    private void Start()
+    {
+        StartCoroutine(ShowFightNumber());
+    }
+
+
+     IEnumerator ShowFightNumber()
+    {
+        int value = 1;
+        while (true)
+        {
+            yield return new WaitForSeconds(interval);
+            FightNumberManager.Instance.ShowFightNumber(value++, transform, (FightNumType)(value % 3));
+        }
+    }
+}
